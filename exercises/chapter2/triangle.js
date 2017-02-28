8c4cab93985d47e6471870a6f70dfa908d42c3f2
Will Palafox <wpalafox@gmail.com>
2017-02-28 13:53:56 -0600

    Triangle exercise

---
+++ exercises/chapter2/triangle.js
@@ -0,0 +1,32 @@
+for( i = 1; i <= 7; i++){ 
+   
+		 if(i<2){ 
+		 	console.log('# \n')
+		 }
+
+		else if(i<3){
+			console.log('## \n')
+		 }
+
+		else if(i<4){
+			console.log('### \n')
+		 }
+
+		else if(i<5){
+			console.log('#### \n')
+		 }
+
+		else if(i<6){
+			console.log('##### \n')
+		 }
+
+
+		else if(i<7){
+			console.log('###### \n')
+		 }
+
+		else if(i<8){
+			console.log('####### \n')
+		 }
+
+}
