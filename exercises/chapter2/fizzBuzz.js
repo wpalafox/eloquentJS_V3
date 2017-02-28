9fa5c36141574556800c8c721651743e9eb77d7f
Will Palafox <wpalafox@gmail.com>
2017-02-28 16:58:47 -0600

    FizzBuzz completed

---
+++ exercises/chapter2/fizzBuzz.js
@@ -0,0 +1,48 @@
+/*
+Write a program that uses console.log to print all the numbers from 1 to 100,
+with two exceptions. For numbers divisible by 3, print "Fizz" instead of the
+number, and for numbers divisible by 5 (and not 3), print "Buzz" instead.
+When you have that working, modify your program to print "FizzBuzz"
+for numbers that are divisible by both 3 and 5.
+(This is actually an interview question that has been claimed to weed
+out a significant percentage of programmer candidates. So if you solved it,
+you’re now allowed to feel good about yourself.)
+*/
+
+
+//My solution 
+
+for(i=1;i<=100;i++){
+	
+	if(i%5==0 && i%3==0){
+		console.log("FizzBuzz");
+	}
+
+	else if(i%3 == 0){
+		console.log("Fizz");
+	}
+
+	else if(i%5==0){
+		console.log("Buzz");
+	} 
+	
+	else{
+		console.log(i);
+	}
+
+}
+
+
+//Author Solution 
+
+for (var n = 1; n <= 100; n++) {
+  var output = "";
+  if (n % 3 == 0)
+    output += "Fizz";
+  if (n % 5 == 0)
+    output += "Buzz";
+  console.log(output || n);
+
+}
+
+//The author's solution won't handle cases where n is divible by 3 AND 5. 
--- exercises/chapter2/triangle.js
+++ exercises/chapter2/triangle.js
@@ -1,3 +1,18 @@
+//Write a loop that makes seven calls to console.log to output the following
+
+/*
+
+#
+##
+###
+####
+#####
+######
+#######
+
+*/
+
+//My solution
 for( i = 1; i <= 7; i++){ 
    
 		 if(i<2){ 
@@ -30,3 +45,12 @@ for( i = 1; i <= 7; i++){
 		 }
 
 }
+
+//Author's solution
+
+for (var line = "#"; line.length < 8; line += "#")
+  console.log(line);
+
+//It's interesting how the for loop lacks brackets, yet it functions perfectly. 
+//Looping without an i variable represents a whole new world for me. 
+//I wonder why/how the line breaks into a new one without "\n"
