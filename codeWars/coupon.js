bf5d9bec944575dca306b0a3e30ea8d0c915e10d
Will Palafox <wpalafox@gmail.com>
2018-04-01 18:57:19 -0500

    Pulling and formatting dates

--- codeWars/coupon.js
+++ codeWars/coupon.js
@@ -1,18 +1,5 @@
 //Coupon Code Challenge
 
-
-//Retrieve the current day"
-	var today = new Date();
-	var dd = today.getDate();
-	var mm = today.getMonth();
-	var yy = today.getFullYear();
-	
-//Change the expiration date to the dd/mm/yyyy format
-
-	var Day = mm+"/"+dd+"/"+yy;
-
-	console.log(Day);
-
 //Extract the expiration Month and convert to corresponding number
 const expirationDate = 'July 1, 2014';
 let exMonthNumber = [];  
@@ -64,28 +51,17 @@ function findMonthNumber(exDate){
 
 findMonthNumber(expirationDate);
 
-
-//Extract the Expiration Day 
-
-
-
-
-
-
-
-
-
-
-
-
-
+let a = parseInt(exMonthNumber);
+console.log(a);
 
 
 
 //Extract Expiration Year from String 
-expirationDate = 'October 1, 2014';
+//const expirationDate = 'October 1, 2014';
 EXY = [];
 rvrsArry = [];
+let exYearNumber;
+
 
 function seeYear(expirationDate){
 
@@ -104,74 +80,36 @@ function reverseArray(array){
 	}
 	var yearNum = Number(rvrsArry.join(""));  
 
-	return yearNum;
+	exYearNumber = yearNum;
 }
 
 
-//Run Program 
+//Run find year functions 
 seeYear(expirationDate);
 reverseArray(EXY);
+let b = exYearNumber;
+console.log(b);
+let exDayAndYear = a + "/" + b;
+//Expiration Month + Year
+console.log(exDayAndYear);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Old code
-
-/*
-function grabMonth(expirationDate){
+//Retrieve the current day"
+	var today = new Date();
+	//var dd = today.getDate();
+	var mm = today.getMonth();
+	var yy = today.getFullYear();
 	
-	str = expirationDate;
+//Change the expiration date to the dd/mm/yyyy format
 
-	EMM = str.charAt(0) +  str.charAt(1) + str.charAt(2)
+	var Day = mm+"/"+yy;
 
-	
+	console.log(Day);
 
-	if(EMM == "Jan"){
-		NEMM = 01;
-	}else if(EMM == "Feb"){
-		NEMM = 02;
-	}else if(EMM == "Mar"){
-		NEMM = 03;
-	}else if(EMM == "Apr"){
-		NEMM = 04;
-	}else if(EMM == "May"){
-		NEMM = 05;
-	}else if(EMM == "Jun"){
-		NEMM = 06;
-	}else if(EMM == "Jul"){
-		NEMM = 07;
-	}else if(EMM == "Aug"){
-		NEMM = 08;
-	}else if(EMM == "Sep"){
-		NEMM = 09;
-	}else if(EMM == "Oct"){
-		NEMM = 10;
-	}else if(EMM == "Nov"){
-		NEMM = 11;
-	}else if(EMM == "Dec"){
-		NEMM = 12;
-	}
-	return NEMM;
 
-}
+//Compare the expiration date and the current date 
 
-grabMonth(expirationDate);
-*/
+
+
 
 
