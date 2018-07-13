0f1edd7fa70207c8a702b88359ac3ac71afc380a
Will Palafox <Will.Palafox@communitybrands.com>
2018-07-13 18:42:51 -0500

    Added description of Vasya file

--- codeWars/Vasya-Clerk.py
+++ codeWars/Vasya-Clerk.py
@@ -1,3 +1,11 @@
+#The new "Avengers" movie has just been released! There are a lot of people at the cinema box office standing in a huge line.
+#Each of them has a single 100, 50 or 25 dollars bill. An "Avengers" ticket costs 25 dollars.
+#Vasya is currently working as a clerk. He wants to sell a ticket to every single person in this line.
+#Can Vasya sell a ticket to each person and give the change if he initially has no money and sells the tickets strictly in the order people follow in the line?
+#Return YES, if Vasya can sell a ticket to each person and give the change with the bills he has at hand at that moment. Otherwise return NO.
+
+
+
 def tickets(people):
     i=0
     twenty_five=0
@@ -33,7 +41,7 @@ def tickets(people):
 
 
 
-#Passes these tests 
-tickets([25,25,50])
-tickets([25,100])
-tickets([25,25,50,50,100])
+#Passes these tests
+tickets([25,25,50])  #YES
+tickets([25,100])  #NO. Vasya would need three 25s or a 25 and a 50 to break change to the 100
+tickets([25,25,50,50,100]) # NO.  Vasya does not have the right bills to give change to the 100 
