da0e0f8cc9ca6b23548d66ff736027483838558a
wpalafox <wpalafox@gmail.com>
2018-09-17 18:52:21 -0500

    Reviewover chapter 4 sum and range exercise

--- exercises/chapter4/reversingAnArray.js
+++ exercises/chapter4/reversingAnArray.js
@@ -1,3 +1,52 @@
+/*Arrays have a reverse method that changes the array by inverting the order 
+in which its elements appear. For this exercise, write two functions, reverseArray 
+and reverseArrayInPlace. The first, reverseArray, takes an array as argument and 
+produces a new array that has the same elements in the inverse order. The second, 
+reverseArrayInPlace, does what the reverse method does: it modifies the array given 
+as argument by reversing its elements. Neither may use the standard reverse method.*/
+
+
+
+function reverseArray(array){
+
+  //somting happen here
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*The first, reverseArry, takes an array as an Argument and produces a 
 new Array that has the same elements in the inverse order */
--- exercises/chapter4/sumOfARange.js
+++ exercises/chapter4/sumOfARange.js
@@ -1,3 +1,53 @@
+//Version 3 Refresh 
+
+/*Write a range function that takes two arguments, 
+start and end, and returns an array containing all 
+the numbers from start up to (and including) end.*/  
+
+function range(a,b, c){
+	ranger=[]
+	if(c==null){ 
+		
+		for(i=a;i<=b;i++){
+			ranger.push(i)
+
+		}
+	
+		
+	}else if(c){
+		//code to implement step value use
+		//how do I handle both positive and negative situations 
+		for(i=a;;i+c){
+			ranger.push(i)
+
+
+		}
+
+
+	return ranger
+	}
+}
+
+/*Next, write a sum function that takes an array of numbers and 
+returns the sum of these numbers. 
+Run the example program and see whether it does indeed return 55.*/
+
+
+function sum(array){
+	sum=0 
+	for(i=0;i<array.length;i++){
+		sum+=array[i]
+
+	}
+
+	return sum
+
+}
+
+
+/*---------------------------------------------------------------------------------------*/
+
+//Version 2 
 /*Write a range function that takes two arguments, start and end, 
 and returns an array containing all the numbers from strart and to
 including) end. */
