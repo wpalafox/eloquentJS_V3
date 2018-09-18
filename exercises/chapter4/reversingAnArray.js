cdf09fac42c8885dfbcf93a5c82c89bf9bb77f36
Will Palafox <Will.Palafox@communitybrands.com>
2018-09-18 08:41:57 -0500

    test

--- exercises/chapter4/reversingAnArray.js
+++ exercises/chapter4/reversingAnArray.js
@@ -1,21 +1,43 @@
-/*Arrays have a reverse method that changes the array by inverting the order 
-in which its elements appear. For this exercise, write two functions, reverseArray 
-and reverseArrayInPlace. The first, reverseArray, takes an array as argument and 
-produces a new array that has the same elements in the inverse order. The second, 
-reverseArrayInPlace, does what the reverse method does: it modifies the array given 
+/*Arrays have a reverse method that changes the array by inverting the order
+in which its elements appear. For this exercise, write two functions, reverseArray
+and reverseArrayInPlace. The first, reverseArray, takes an array as argument and
+produces a new array that has the same elements in the inverse order. The second,
+reverseArrayInPlace, does what the reverse method does: it modifies the array given
 as argument by reversing its elements. Neither may use the standard reverse method.*/
 
+arraytest=[1,2,3,4,5,6,7,8,9]
 
-
-function reverseArray1(array){
-  let output=[] 
+function reverseArray0(array){
+  let output=[]
   for(i=-1;i<=array.length;i++){
     output.push(array.pop())
   }
 
-  return output 
+  return output
+/*The code above does not work because array.pop() removes the popped element
+from the array, reducing its size by one, so the length is reducing.
+The code below resolves this problem.  While arrayTest.length evaluates
+to True, run*/
 
 
+function reverseArray1(array){
+  let output=[]
+  while(arrayTest.length){
+    output.push(array.pop())
+  }
+
+  return output
+
+}
+reverseArray1(arrayTest)
+
+
+
+
+
+
+/*simply go over the input array from front to back and use
+the unshift method on the new array to insert each element at its start.*/
 
 
 
@@ -52,13 +74,13 @@ function reverseArray1(array){
 
 
 
-/*The first, reverseArry, takes an array as an Argument and produces a 
+/*The first, reverseArry, takes an array as an Argument and produces a
 new Array that has the same elements in the inverse order */
 var testA = [3,6,9,12];
 
 
-function reverseA(array){ 
-	
+function reverseA(array){
+
 	let rvrseArry = [];
  	for(var i=array.length-1; i>=0; i--) {
 		rvrseArry.push(testA[i])
@@ -71,7 +93,7 @@ function reverseA(array){
 reverseA(testA);
 
 /* The second, reverseArrayInPlace, does what the reverse method does:
-it modifies the array given as argument in order to reverse its elements. 
+it modifies the array given as argument in order to reverse its elements.
 Neither may use the standard reverse method */
 
 /*Book Answer */
