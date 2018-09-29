2038769fd15b9bbed0e6f9a57e9f7a589c6942bb
wpalafox <wpalafox@gmail.com>
2018-09-28 21:10:07 -0500

    finish longest.js

--- exercises/chapter4/aList.js
+++ exercises/chapter4/aList.js
@@ -48,12 +48,47 @@ takes an element and a list and creates a new list that adds the element to
 the front of the input list, and nth, which takes a list that adds the 
 element to the fron of the input list, and nth.*/
 
+/*Every iteration of the loop, node points to the current sublist, and the 
+body can read its value property to get the current element.*/
+
+function listToArray(list){
+  let array=[]
+  for (let node=list; node; node= node.rest){
+  	//reading value prop, to get current element 
+  	array.push(node.value)
+
+  }
+
+  return array 
+
+}
 
 
+/*Add a helper function prepend, which takes an elemental and a list and 
+creates a new list that adds the element to the front of the input list*/
 
 
+function prepend(value, list){
+
+  return {value, rest: list}; 
+
+}
 
 
+/*Take a list and a number and returns the element at the given position 
+in the list (with zero referring to the first element) or undefined when
+there is no such element*/
+
+function nth(list, n){
+	if (!list) return undefined;
+	else if (n == 0) return list.value;
+	else return nth(list.rest, n-1); 
+
+}
+
+function nth(list, n){
+	for(node=list)
 
 
+}
 
