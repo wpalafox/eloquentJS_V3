194de6bec073f4f686ef3375404e9e68b4aeb679
Will Palafox <Will.Palafox@communitybrands.com>
2018-10-19 12:39:17 -0500

    Isogram exercise complete

--- codeWars/isIsogram.js
+++ codeWars/isIsogram.js
@@ -4,20 +4,18 @@ consecutive. Implement a function that determines whether a string that contains
  only letters is an isogram. Assume the empty string is an isogram. Ignore
   letter case.
 */
-
-
+//Idea, take the string and lowercase everything, then evaluate
+//Pass
 function isIsogram(str){
 
-  let string = str
-  console.log(string.length)
+  let string = str.toLowerCase()
+  console.log(string)
   arr=[]
 
   for(i=0; i< string.length; i++){
-
     if(arr.includes(string[i])==false){
       arr.push(string[i])
-    }else if(arr.includes(string[i]) || Number.isInteger(parseInt(string[i])))
-    {
+    }else if(arr.includes(string[i])){
       return false
     }
 }
@@ -27,5 +25,20 @@ return true
 
 }
 
+//Codewars soltution
+function isIsogram(str){
+  var i, j;
+  str = str.toLowerCase();
+  for(i = 0; i < str.length; ++i)
+    for(j = i + 1; j < str.length; ++j)
+      if(str[i] === str[j])
+        return false;
+  return true;
+}
 
-isIsogram("mo0se")
+
+
+
+
+
+isIsogram("mooOaaaAAAbbbBBBcccCCCe")
