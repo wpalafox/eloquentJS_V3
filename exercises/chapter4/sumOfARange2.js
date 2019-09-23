4a182e2cba60e26e2486f831b16976965a4f6cfb
wpalafox <wpalafox@gmail.com>
2019-09-23 15:43:18 -0500

    sum of range redone

--- exercises/chapter4/sumOfARange2.js
+++ exercises/chapter4/sumOfARange2.js
@@ -16,7 +16,7 @@ let range = (a,b) => {
 	tempArry = [];
 
 	for(let i = a; i <= b; i++){
-		tempArry += i+" ";
+		tempArry.push(i);
 
 	}
 
@@ -25,10 +25,22 @@ let range = (a,b) => {
 
 }
 
-console.log(range(10,15))
+console.log(range(3,5))
+
+let sum = (array) => {
+  total=0;
+
+  for(i=0;i<array.length;i++){
+  	total += array[i]
+  }
+
+  return total 
 
 
+}
 
+console.log(sum(range(3,5)))
 /* Next, write a sum function that takes an array of numbers and returns 
 the sum of these numbers. Run the example program and see whether it does
  indeed return 55. */
+
